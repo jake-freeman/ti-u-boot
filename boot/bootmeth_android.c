thom24/u-boot

commit 1c6cf852b8b8f869d2c5e39eb071ec3025bf739a

TI: bootstd: android: Implement DTBO support
Android allows bootloaders to apply DTBO (Device Tree Blob Overlays)
in order to configure additional hardware.

This standard practice documented at [1]

If the board defines an overlay address at "fdtoverlay_addr_r",
then we read from the dtbo partition and let image-fdt.c apply the
overlay before booting.

[0] https://source.android.com/docs/core/architecture/dto
Signed-off-by: Mattijs Korpershoek <mkorpershoek@baylibre.com>

boot/bootmeth_android.c
@@ -451,6 +451,7 @@ static int boot_android_normal(struct bootflow *bflow)
 	int ret;
 	ulong loadaddr = env_get_hex("loadaddr", 0);
 	ulong vloadaddr = env_get_hex("vendor_boot_comp_addr_r", 0);
+	ulong fdtoverlay_addr_r = env_get_hex("fdtoverlay_addr_r", 0);
 
 	ret = run_avb_verification(bflow);
 	if (ret < 0)
@@ -471,6 +472,10 @@ static int boot_android_normal(struct bootflow *bflow)
 
 	set_abootimg_addr(loadaddr);
 	set_avendor_bootimg_addr(vloadaddr);
+
+	ret = read_slotted_partition(desc, "dtbo", priv->slot, fdtoverlay_addr_r);
+	if (ret < 0)
+		return log_msg_ret("read dtbo", ret);
 
 	ret = bootm_boot_start(loadaddr, bflow->cmdline);
 

boot/image-fdt.c
@@ -15,6 +15,7 @@
 #include <env.h>
 #include <errno.h>
 #include <image.h>
+#include <image-android-dt.h>
 #include <lmb.h>
 #include <log.h>
 #include <malloc.h>
@@ -512,6 +513,33 @@ int boot_get_fdt(void *buf, const char *select, uint arch,
 				goto no_fdt;
 
 			debug("## Using FDT in Android image dtb area with idx %u\n", dtb_idx);
+
+#ifdef CONFIG_OF_LIBFDT_OVERLAY
+			if (env_get("adtbo_idx")) {
+				ulong dtbo_addr;
+				u32 dtbo_size;
+				u32 dtbo_index = (u32)env_get_ulong("adtbo_idx", 10, 0);
+				ulong fdtoverlay_addr_r = env_get_hex("fdtoverlay_addr_r", 0);
+				int ret;
+
+				ret = android_dt_get_fdt_by_index(fdtoverlay_addr_r, dtbo_index,
+								  &dtbo_addr, &dtbo_size);
+				if (ret < 0)
+					goto error;
+
+				ret = fdt_shrink_to_minimum(fdt_blob, dtbo_size);
+				if (ret < 0)
+					goto error;
+
+				debug("## Found Android dtbo %d with addr %lx and size %u\n",
+				      dtbo_index, dtbo_addr, dtbo_size);
+
+				ret = fdt_overlay_apply_verbose(fdt_blob,
+								map_sysmem(dtbo_addr, 0));
+				if (ret < 0)
+					goto error;
+			}
+#endif
 		} else if (!android_image_get_second(hdr, &fdt_data, &fdt_len) &&
 			!fdt_check_header((char *)fdt_data)) {
 			fdt_blob = (char *)fdt_data;

jake-freeman/ti-u-boot

commit 5f618362f111fed68d3c540ac8154961e8da710c

TI: bootstd: android: fix dtbo implementation
When DTBO support was added we miss to add in kernel commandline:
androidboot.dtbo_idx=XX

Create a function to read adtbo_idx variable and if we find a value add
it in kernel command line.

Fixes: 1c6cf852b8b8 ("TI: bootstd: android: Implement DTBO support")
Signed-off-by: Guillaume La Roque <glaroque@baylibre.com>

boot/bootmeth_android.c
@@ -171,6 +171,16 @@ static int configure_serialno(struct bootflow *bflow)
 	return bootflow_cmdline_set_arg(bflow, "androidboot.serialno", serialno, false);
 }
 
+static int configure_dtbo_idx(struct bootflow *bflow)
+{
+	char *adtbo_idx = env_get("adtbo_idx");
+
+	if (!adtbo_idx)
+		return log_msg_ret("dtbo", -ENOENT);
+
+	return bootflow_cmdline_set_arg(bflow, "androidboot.dtbo_idx", adtbo_idx, false);
+}
+
 static int android_read_bootflow(struct udevice *dev, struct bootflow *bflow)
 {
 	struct blk_desc *desc = dev_get_uclass_plat(bflow->blk);
@@ -264,8 +274,12 @@ static int android_read_bootflow(struct udevice *dev, struct bootflow *bflow)
 		goto free_priv;
 	}
 
-	/* Ignoring return code: setting serial number is not mandatory for booting */
+	/*
+	 * Ignoring return code: setting serial number and
+	 * dtbo index are not mandatory for booting
+	 */
 	configure_serialno(bflow);
+	configure_dtbo_idx(bflow);
 
 	if (priv->boot_mode == ANDROID_BOOT_MODE_NORMAL) {
 		ret = bootflow_cmdline_set_arg(bflow, "androidboot.force_normal_boot",